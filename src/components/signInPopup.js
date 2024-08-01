af2ed1a857461c33232592fb24a2ffc117c59df3
Oscapoco <okpoco15@gmail.com>
2024-08-01 09:20:24 +0200

    update

--- src/components/signInPopup.js
+++ src/components/signInPopup.js
@@ -1,47 +1,37 @@
 import './signInPopup.css';
+// import { useState } from 'react';
 import axios from 'axios';
-import { useState } from 'react';
 
 function SignInPopup({ setIsAuthenticated }) {
-    const [email, setEmail] = useState('');
-    const [password, setPassword] = useState('');
+    
 
-    const handleSignIn = async (e) => {
-        e.preventDefault();
-        try {
-            const response = await axios.get(`http://localhost:3004/users?email=${email}&password=${password}`);
-            if (response.data.length > 0) {
-                // Generate a simple token (in a real app, this should be done on the server)
-                const token = btoa(email + ':' + password);
-                localStorage.setItem('authToken', token);
+    const onFinish = values =>{
+        const {username, password} = values
+        axios.post('http://localhost:3004/users', {username, password})
+        .then(res =>{
+            if(res.data.validation){
                 setIsAuthenticated(true);
-                alert('Login successful');
-            } else {
-                alert('Invalid email or password');
-            }
-        } catch (error) {
-            console.error('Error signing in:', error);
-            alert('Error signing in');
-        }
-    };
+            }else{console.log('Wrong credentials')}
+        })
+    }
 
     return (
         <div className='signInPopup'>
             <div className='title'>Sign In</div>
-            <form onSubmit={handleSignIn}>
-                <input 
-                    type='email' 
-                    value={email} 
-                    onChange={(e) => setEmail(e.target.value)} 
-                    placeholder='Email' 
-                    required 
+            <form onClick={onFinish}>
+                <input
+                    type='text'
+                    name='username'
+                    placeholder='Email'
+                   
+                    required
                 />
-                <input 
-                    type='password' 
-                    value={password} 
-                    onChange={(e) => setPassword(e.target.value)} 
-                    placeholder='Password' 
-                    required 
+                <input
+                    type='password'
+                    name='password'
+                    placeholder='Password'
+                
+                    required
                 />
                 <button type='submit'>Submit</button>
             </form>
