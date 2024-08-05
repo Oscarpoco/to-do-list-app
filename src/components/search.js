e6c994bbbb0493be59174272ba6e7dd0b53d2dc4
Oscapoco <okpoco15@gmail.com>
2024-08-05 14:37:49 +0200

    updated

--- src/components/search.js
+++ src/components/search.js
@@ -61,9 +61,12 @@ const styles = {
     height: '50%', 
   },
   icon: {
-    fontSize: '1.8rem',
+    fontSize: '1.5rem',
     cursor: 'pointer',
     color: 'black',
+    border: '1px solid lightblue',
+    borderRadius: '50%',
+    padding: '5px 5px'
   },
   input: {
     width: '70%',
